d2b09647106ed44b1432f6f4853257b35d1ed2ec
17931837B <tomoya.bb1014@gmail.com>
2025-07-20 17:27:47 +0900

    背景を白にした

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,44 +4,48 @@
 # include <glm/glm.hpp>
 # include <iostream>
 
-const unsigned int WINDOW_WIDTH = 1440;
-const unsigned int WINDOW_HEIGHT = 810;
+const unsigned int WINDOW_WIDTH = 800;
+const unsigned int WINDOW_HEIGHT = 640;
 
 void keyHandler(GLFWwindow*, int, int, int, int);
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HelloOpenGL", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HelloOpenGL", nullptr, nullptr);
 
-    if (!window)
-    {
-        std::cerr << "Failed to create window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, keyHandler);
+	if (!window)
+	{
+		std::cerr << "Failed to create window." << std::endl;
+		glfwTerminate();
+		return (-1);
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, keyHandler);
 
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-    glfwTerminate();
-    return 0;
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
 
 void keyHandler(GLFWwindow *window, int key, int scancode, int action, int mods)
