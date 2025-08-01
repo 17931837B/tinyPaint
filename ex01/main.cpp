49f7c43750969000d0f436b03e27d1a0f97eb57c
17931837B <tomoya.bb1014@gmail.com>
2025-08-01 11:36:40 +0900

    ex01 doing

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -26,15 +26,15 @@ ImageData* createRedImage(int width, int height)
 	img->width = width;
 	img->height = height;
 	img->imageData = new unsigned char[width * height * 4];
-	i = 0;
-	while (i < width * height)
-	{
-		img->imageData[i * 4 + 0] = 255;
-		img->imageData[i * 4 + 1] = 0;
-		img->imageData[i * 4 + 2] = 0;
-		img->imageData[i * 4 + 3] = 255;
-		i++;
-	}
+	// i = 0;
+	// while (i < width * height)
+	// {
+	// 	img->imageData[i * 4 + 0] = 255;
+	// 	img->imageData[i * 4 + 1] = 0;
+	// 	img->imageData[i * 4 + 2] = 0;
+	// 	img->imageData[i * 4 + 3] = 255;
+	// 	i++;
+	// }
 	return (img);
 }
 
@@ -69,58 +69,55 @@ void	framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
 void otherInit(void)
 {
-    // ウィンドウの背景色を青に設定
-    glClearColor(0.0f, 0.0f, .0f, 1.0f); 
-    glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f); 
+	glEnable(GL_DEPTH_TEST); //深度テスト手前のみを描写
 }
 
 void LoadTexture()
 {
-    globalImg = createRedImage(4096, 4096);
+	int	width;
+	int	height;
 
-    glGenTextures(1, &texId);
-    glBindTexture(GL_TEXTURE_2D, texId);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, globalImg->width, globalImg->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, globalImg->imageData);
-    glGenFramebuffers(1, &fboId);
-    glBindFramebuffer(GL_FRAMEBUFFER, fboId);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texId, 0);
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        std::cerr << "Framebuffer is not complete!" << std::endl;
-    glViewport(0, 0, globalImg->width, globalImg->height);
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // フレームバッファをクリアするための色を赤に設定
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0); 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height); 
-    glViewport(0, 0, width, height);
-    glDeleteFramebuffers(1, &fboId);
-
-    // 修正箇所: フレームバッファのクリア後、ウィンドウの背景色を再度設定し直す
-    // これをしないと、display()でのglClearが赤でクリアしてしまう
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
+	globalImg = createRedImage(4096, 4096);
+	glGenTextures(1, &texId);
+	glBindTexture(GL_TEXTURE_2D, texId);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, globalImg->width, globalImg->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, globalImg->imageData);
+	glGenFramebuffers(1, &fboId);
+	glBindFramebuffer(GL_FRAMEBUFFER, fboId);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texId, 0);
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		std::cerr << "Framebuffer is not complete!" << std::endl;
+	glViewport(0, 0, globalImg->width, globalImg->height);
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glfwGetFramebufferSize(window, &width, &height); 
+	glViewport(0, 0, width, height);
+	glDeleteFramebuffers(1, &fboId);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f); 
 }
 
-void display() 
+void	display() 
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-    glLoadIdentity();
-    glBindTexture(GL_TEXTURE_2D, texId);
-    glEnable(GL_TEXTURE_2D);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.f, 0.f); glVertex3f(-1.0f, -1.0f, 0.f);
-    glTexCoord2f(1.f, 0.f); glVertex3f( 1.0f, -1.0f, 0.f);
-    glTexCoord2f(1.f, 1.f); glVertex3f( 1.0f,  1.0f, 0.f);
-    glTexCoord2f(0.f, 1.f); glVertex3f(-1.0f,  1.0f, 0.f);
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glLoadIdentity();
+	glBindTexture(GL_TEXTURE_2D, texId);
+	glEnable(GL_TEXTURE_2D);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.f, 0.f); glVertex3f(-1.0f, -1.0f, 0.f);
+	glTexCoord2f(1.f, 0.f); glVertex3f( 1.0f, -1.0f, 0.f);
+	glTexCoord2f(1.f, 1.f); glVertex3f( 1.0f,  1.0f, 0.f);
+	glTexCoord2f(0.f, 1.f); glVertex3f(-1.0f,  1.0f, 0.f);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
 }
 
 int main(int argc, char **argv)
@@ -136,7 +133,7 @@ int main(int argc, char **argv)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); 
-    window = glfwCreateWindow(600, 900, "OpenGL Exercise 01", NULL, NULL); 
+    window = glfwCreateWindow(4096, 4096, "OpenGL Exercise 01", NULL, NULL); 
     if (!window)
     {
         std::cerr << "Failed to create GLFW window" << std::endl;
